c7c909eeab3adf561eca93e94b87e66bca2a7d7c
Kacper Gunia <cakper@gmail.com>
2013-05-30 16:40:40 +0100

    PHPSpec Update

--- spec/Day01/CalcStatsSpec.php
+++ spec/Day01/CalcStatsSpec.php
@@ -2,9 +2,10 @@
 
 namespace spec\Day01;
 
-use PHPSpec2\ObjectBehavior;
+use PhpSpec\ObjectBehavior;
+use Prophecy\Argument;
 
-class CalcStats extends ObjectBehavior
+class CalcStatsSpec extends ObjectBehavior
 {
     private $numbers = array(6, 9, 15, -2, 92, 11);
 
@@ -13,22 +14,22 @@ class CalcStats extends ObjectBehavior
         $this->shouldHaveType('Day01\CalcStats');
     }
 
-    function it_should_return_minimum_value()
+    function it_returns_minimum_value()
     {
         $this->min($this->numbers)->shouldReturn(-2);
     }
 
-    function it_should_return_maxmium_value()
+    function it_returns_maxmium_value()
     {
         $this->max($this->numbers)->shouldReturn(92);
     }
 
-    function it_should_return_number_of_elements_in_the_sequence()
+    function it_returns_number_of_elements_in_the_sequence()
     {
         $this->count($this->numbers)->shouldReturn(6);
     }
 
-    function it_should_return_average_value()
+    function it_returns_average_value()
     {
         $this->avg($this->numbers)->shouldReturn(21.833333);
     }
